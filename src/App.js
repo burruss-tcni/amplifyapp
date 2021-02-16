c925d5f62090e40d260a09acc0de7c6d50a81c8a
burruss-tcni <burruss@tcni.net>
2021-02-16 09:35:21 -0500

    Added App-note for TCNI

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload!
         </p>
+	<p className="App-note">
+	    Testing AWS Amplify CI/CD Web App for TCNI...
+	</p>
       </div>
     );
   }
